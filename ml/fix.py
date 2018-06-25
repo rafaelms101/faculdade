c3616687b5cd609264262e6a64b932a3e9a1389e
Rafael Martins <redmajor@gmail.com>
2018-06-25 09:32:37 -0300

    TP final

--- ml/fix.py
+++ ml/fix.py
@@ -1,7 +1,7 @@
-file = open('dota_data_old.txt')
+file = open('dota_old.txt')
 str = file.read()
 lines = str.split('\n')
 for line in lines:
     ints = line.split()
-    if len(ints) == 13:
+    if len(ints) != 13:
         print(line)
